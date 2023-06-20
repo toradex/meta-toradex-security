7dbcad3e43318def1a708c9361da36e3e3b30656
Sergio Prado <sergio.prado@toradex.com>
2023-06-20 11:23:13 -0300

    imx-mkimage: create file with fuse commands
    When signing the image, also creates a file with fuse commands.
    
    This might be helpful to (brave) users that want to test the signed
    image, and might be useful as well when implementing provisioning with
    Easy Installer.
    
    The file is generated at ${DEPLOY_DIR_IMAGE}/fuse-cmds.txt. Example:
    
    $ cat deploy/images/verdin-imx8mp/fuse-cmds.txt
    # These are One-Time Programmable e-fuses. Once you write them you can't
    # go back, so get it right the first time!
    fuse prog -y 6 0 0x8AE322B2
    fuse prog -y 6 1 0xDF2939A3
    fuse prog -y 6 2 0x9DA80323
    fuse prog -y 6 3 0x3B024EF2
    fuse prog -y 7 0 0xA53091
    fuse prog -y 7 1 0x55304E7A
    fuse prog -y 7 2 0xFB8FF259
    fuse prog -y 7 3 0x9CE57582
    
    # After the device successfully boots a signed image without generating
    # any HAB events, it is safe to secure, or 'close', the device. This is
    # the last step in the process. Once the fuse is blown, the chip does
    # not load an image that has not been signed using the correct PKI tree.
    # Be careful! This is again a One-Time Programmable e-fuse. Once you
    # write it you can't go back, so get it right the first time. If
    # anything in the previous steps wasn't done correctly, after writing
    # this bit, the SOM will not boot anymore!
    fuse prog -y 1 3 0x02000000
    
    Related-to: TOR-1701
    
    Signed-off-by: Sergio Prado <sergio.prado@toradex.com>

---
+++ recipes-bsp/imx-mkimage/files/create_fuse_cmds.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+# parameters
+SOC="$1"
+SRK_FUSE_FILE="$2"
+FUSE_CMDS_FILE="$3"
+
+# warning to program SRK hash fuses
+WARNING1="\
+# These are One-Time Programmable e-fuses. Once you write them you can't
+# go back, so get it right the first time!"
+
+# warning to 'close' the device
+WARNING2="\
+# After the device successfully boots a signed image without generating
+# any HAB events, it is safe to secure, or 'close', the device. This is
+# the last step in the process. Once the fuse is blown, the chip does
+# not load an image that has not been signed using the correct PKI tree.
+# Be careful! This is again a One-Time Programmable e-fuse. Once you
+# write it you can't go back, so get it right the first time. If
+# anything in the previous steps wasn't done correctly, after writing
+# this bit, the SOM will not boot anymore!"
+
+fuse_write_line() {
+    bank=$1
+    word=$2
+    hexval=$3
+    echo "fuse prog -y $bank $word $hexval"
+}
+
+create_fuse_cmds_mx8m() {
+    echo "${WARNING1}"
+
+    # commands to program SRK_HASH fuses
+    bank=6
+    word=0
+    for hexval in $(hexdump -e '/4 "0x"' -e '/4 "%X""\n"' ${SRK_FUSE_FILE}); do
+        fuse_write_line $bank $word $hexval
+        word=$((word+1))
+        if [ "$word" = "4" ]; then
+            bank=$((bank+1))
+            word=0
+        fi
+    done
+
+    # command to program SEC_CONFIG fuse and 'close' the device
+    echo -e "\n${WARNING2}"
+    fuse_write_line 1 3 0x02000000
+}
+
+case ${SOC} in
+    mx8m)
+        create_fuse_cmds_mx8m > ${FUSE_CMDS_FILE}
+        ;;
+    *)
+        echo "Invalid SOC!"
+        return 1
+        ;;
+esac
